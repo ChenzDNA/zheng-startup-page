556fea411fb25c2b653d454c6a6a7099f7488336
陈征 <1007324849@qq.com>
2021-09-19 22:06:01 +0800

    待办完成

---
+++ src/components/center-components/todo/SingleTodo.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="single-todo">
+    <div
+      @click="data.finished = !data.finished"
+      v-text="`${data.finished ? '已完成' : '未完成'}`"
+      :class="`${data.finished ? 'todo-back' : 'todo-finish'}`"
+    ></div>
+    <div v-html="data.content" :class="`${data.finished ? 'done' : ''}`"></div>
+    <div v-html="data.timeText"></div>
+    <div class="delete-todo" @click="deleteTodo">删除</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleTodo",
+  props: {
+    data: Object,
+  },
+  methods: {
+    deleteTodo() {
+      this.data.visible = false;
+    },
+  },
+};
+</script>
+
+<style>
+.single-todo {
+  height: 50px;
+  vertical-align: middle;
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+.single-todo:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+.single-todo > div {
+  height: 50px;
+  display: inline-block;
+  line-height: 50px;
+  transition: 0.2s;
+}
+.single-todo > div:nth-child(1) {
+  width: 50px;
+  border-radius: 10px;
+  user-select: none;
+  cursor: pointer;
+}
+.single-todo > div:nth-child(2) {
+  flex: 1;
+  position: relative;
+  padding-left: 10px;
+}
+.single-todo > div:nth-child(3) {
+  width: 80px;
+}
+.done {
+  text-decoration-line: line-through;
+}
+.delete-todo {
+  padding: 0 10px;
+  user-select: none;
+  cursor: pointer;
+  border-radius: 10px;
+}
+.delete-todo:hover {
+  background-color: rgba(250, 75, 60, 0.7);
+}
+.todo-finish:hover {
+  background-color: rgba(10, 255, 100, 0.5);
+}
+.todo-back:hover {
+  background-color: rgba(250, 105, 100, 0.5);
+}
+</style>
--- src/components/center-components/todo/Todo.vue
+++ src/components/center-components/todo/Todo.vue
@@ -1,12 +1,121 @@
 <template>
-  <div id="todo">diao</div>
+  <div id="todo">
+    <div id="todo-list" class="beautify-scrollbar">
+      <SingleTodo
+        v-for="(item, index) in todoItems"
+        :key="index"
+        :data="item"
+      ></SingleTodo>
+    </div>
+    <div id="todo-finished" class="beautify-scrollbar">
+      <SingleTodo
+        v-for="(item, index) in finishedItems"
+        :key="index"
+        :data="item"
+      ></SingleTodo>
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleTodo from "./SingleTodo.vue";
+
 export default {
   name: "Todo",
+  components: {
+    SingleTodo,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          content: "吃饭",
+          timeText: "11月11日",
+          finished: false,
+          needAlarm: false,
+          visible: true,
+          index: 0,
+          ctime: "",
+          mtime: "",
+        },
+        {
+          id: 0,
+          content: "睡觉",
+          timeText: "11月11日",
+          finished: false,
+          needAlarm: false,
+          visible: true,
+          index: 1,
+          ctime: "",
+          mtime: "",
+        },
+        {
+          id: 0,
+          content: "打豆豆",
+          timeText: "11月11日",
+          finished: false,
+          needAlarm: false,
+          visible: true,
+          index: 2,
+          ctime: "",
+          mtime: "",
+        },
+        {
+          id: 0,
+          content: "睡觉02",
+          timeText: "11月11日",
+          finished: true,
+          needAlarm: false,
+          visible: true,
+          index: 3,
+          ctime: "",
+          mtime: "",
+        },
+        {
+          id: 0,
+          content: "魔界战记",
+          timeText: "11月1日",
+          finished: true,
+          needAlarm: false,
+          visible: true,
+          index: 4,
+          ctime: "",
+          mtime: "",
+        },
+      ],
+    };
+  },
+  computed: {
+    todoItems() {
+      return this.items.filter((item) => !item.finished && item.visible);
+    },
+    finishedItems() {
+      return this.items.filter((item) => item.finished && item.visible);
+    },
+  },
 };
 </script>
 
 <style>
+#todo {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  overflow: hidden;
+}
+#todo > * {
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+#todo-list {
+  border-right: 1px solid #8adaee;
+  flex: 1;
+  height: 100%;
+}
+#todo-finished {
+  border-left: 1px solid #8adaee;
+  flex: 1;
+  height: 100%;
+}
 </style>
