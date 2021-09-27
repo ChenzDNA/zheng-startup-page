8cbbd8341758cfb3fcc2209fe86c1751e1adf696
陈征 <1007324849@qq.com>
2021-09-27 20:58:47 +0800

    使用vuex

--- src/store.js
+++ src/store.js
@@ -4,38 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  timeConf: {
-    second: true,
-    fontColor: "#a2f58f",
-  },
-  searchConf: {
-    searchEngine: "bing",
-    searchEngines: {
-      bing: "https://www.bing.com/search?mkt=zh-CN&q=",
-      baidu: "https://www.baidu.com/s?ie=UTF-8&wd=",
-    },
-  },
-  weatherConf: {
-    cityName: "湘潭",
-    tempHigh: "34℃",
-    tempLow: "24℃",
-    windDirection: "北风",
-    windStrength: "<3级",
-    weather: "晴",
-    infoText: "",
-  },
-  curtainConf: {
-    curtain: false,
-  },
-  sidebarConf: {
-    realRight: "-300px",
-  },
+  
 }
 const getters = {
 
 }
 const mutations = {
-  
+
 }
 const actions = {
 
