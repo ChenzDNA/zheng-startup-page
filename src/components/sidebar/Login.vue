20ac4366c5c96008bc8fd5d055fc7b9d7e57f28a
陈征 <1007324849@qq.com>
2021-09-29 15:16:43 +0800

    调样式

--- src/components/sidebar/Login.vue
+++ src/components/sidebar/Login.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="login">
     <label for="user-name">用户名：</label>
-    <input type="text" id="user-name" />
+    <input type="text" id="account" v-model="account" />
     <br />
     <label for="password">密&emsp;码：</label>
-    <input type="password" id="password" />
+    <input type="password" id="password" v-model="password" />
     <br />
     <div>登录</div>
     <div>注册</div>
@@ -16,7 +16,7 @@ export default {
   name: "Login",
   data() {
     return {
-      userName: "",
+      account: "",
       password: "",
     };
   },
@@ -30,8 +30,9 @@ export default {
   text-align: center;
   cursor: pointer;
   user-select: none;
+  margin-top: 10px;
 }
-#login>div:hover{
+#login > div:hover {
   backdrop-filter: blur(3px);
   -webkit-backdrop-filter: blur(3px);
 }
