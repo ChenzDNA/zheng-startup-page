357b29759d11e5c830a5fb4bdee88877bfdef650
陈征 <1007324849@qq.com>
2021-10-10 20:30:15 +0800

    把请求数据用 FormData 发送

--- src/util/request.js
+++ src/util/request.js
@@ -1,29 +1,35 @@
 import axios from 'axios'
 
-export function request(config) {
-
-  //1.创建axios实例
+export default function request(config) {
+  // 1.创建axios实例
   const instance = axios.create({
     baseURL: '/api',
     timeout: 5000
   })
 
-  //2.axois的拦截器(interceptors)
-  //2.1.请求拦截
+  // 2.axois的拦截器(interceptors)
+  // 2.1.请求拦截
   instance.interceptors.request.use(config => {
     return config
   })
 
-  //2.2.响应拦截
+  // 2.2.响应拦截
   instance.interceptors.response.use(
     res => {
       return res.data
     },
     err => {
+      console.log(err)
     }
   )
 
-  //3.发送真正的网络请求   
-  return instance(config)
+  // 把数据转成 FormData
+  let data = new FormData
+  for (let key of config.data) {
+    data.append(key, config.data[key])
+  }
+  config.data = data
 
+  // 3.发送真正的网络请求
+  return instance(data)
 }
