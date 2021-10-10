ba000dff5a33f73fde3a049a6d63ea7ccbfc4ee5
陈征 <1007324849@qq.com>
2021-10-10 22:17:15 +0800

    登录修改状态

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,13 @@
 export default {
-  
+  updateUserState(state, data) {
+    if (data.code != 200) {
+      state.loginMsg = data.message
+      return
+    }
+    state.userState.isLogin = true
+    state.userState.userName = data.data.userData.nickname
+  },
+  updateLoginMsg(state, msg) {
+    state.loginMsg = msg;
+  }
 }
