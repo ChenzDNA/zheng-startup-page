67746338e591cf1422c34680c40d504fb0a6237d
陈征 <1007324849@qq.com>
2021-10-10 21:17:43 +0800

    完成登录和注册接口

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,29 @@
+import request from '../util/request'
+
 export default {
-  
+  userLogin({ commit }, data) {
+    if (!data) {
+      data = new Object
+    }
+    request({
+      url: '/user/login',
+      data,
+      method: 'post',
+    }).then(res => {
+      commit('userLogin', res)
+    })
+  },
+  userRegister({ commit }, data) {
+    console.log('in register')
+    request({
+      url: '/user/register',
+      data,
+      method: 'post',
+    }).then(res => {
+      commit('userRegister', res)
+    })
+  },
+  updateLoginMsg({ commit }, msg) {
+    commit('updateLoginMsg', msg)
+  },
 }
